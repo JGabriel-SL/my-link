1bf548ac661073feb6c346933be9f92131961283
Joao Gabriel <gabrielsl.alu.lmb@gmail.com>
2022-02-03 17:55:24 -0300

    pushing the project start

---
+++ src/pages/Home/index.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div className="container-home">
+        <div className="logo">
+            <img src="/logo.png" alt="Sujeito Link Logo" />
+            <h1>SujeitoLink</h1>
+            <span>Cole seu link para encurtar 👇🏻</span>
+        </div>
+
+        <div className="area-input">
+
+        </div>
+    </div>
+  );
+}
---
+++ src/pages/Links/index.js
@@ -0,0 +1,7 @@
+export default function Links() {
+  return (
+    <div>
+        <h1>PAGE LINKS</h1>
+    </div>
+  );
+}
